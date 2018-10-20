27b3e0c2f36a4edb609692913cdc8d0123d218db
jatin singhal <jatin.singhal@wdc.com>
2018-10-20 13:20:06 +0530

    modified deploy file with hello world flask-ask app

--- deploy.py
+++ deploy.py
@@ -1,9 +1,51 @@
+# from flask import Flask, render_template
+# from flask_ask import Ask, statement, question, session
+# from datetime import datetime
+# import requests
+# import urllib
+# import re
+
+
+# app = Flask(__name__)
+# ask = Ask(app, "/alexa")
+
+import logging
+import os
 from flask import Flask
+from flask_ask import Ask, request, session, question, statement
+
 
 app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 
-@app.route('/')
-def index():
-	return '<h1>Deployed tp Heroku!!!</h1>'
+@ask.launch
+def launch():
+    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
 
+
+@ask.intent('HelloWorldIntent')
+def hello_world():
+    speech_text = 'Hello world'
+    return statement(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can say hello to me!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+
+
+if __name__ == '__main__':
+    if 'ASK_VERIFY_REQUESTS' in os.environ:
+        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
+        if verify == 'false':
+            app.config['ASK_VERIFY_REQUESTS'] = False
+app.run(debug=True)
