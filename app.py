30379db309790fd2582e06a612839ab84b8946ca
jatin singhal <jatin.singhal@wdc.com>
2018-10-22 18:12:33 +0530

    modified app file

--- app.py
+++ app.py
@@ -11,18 +11,10 @@ ask = Ask(app, "/alexa")
 
 @ask.launch
 def new_ask():
-    # welcome = render_template('welcome')
-    # return question(welcome) \
-    #     .simple_card(title='Launch page', content='Make your time')
     print("Launch invoked")
+
     return question("Hi Jatin")
 
-@ask.intent('intro')
-def introduction(name):
-    # dummy_string = "Hi " + name + ". Its good to see you."
-    # return question(dummy_string) \
-    #     .simple_card(title=' Intro page...', content='Make your time')
-    return statement("yo")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
