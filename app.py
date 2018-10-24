1e1a56318e01f9af0bfdae77966e2570b9877c5a
jatin singhal <jatin.singhal@wdc.com>
2018-10-25 00:13:32 +0530

    modified app file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_ask import Ask, statement, question, session
 from datetime import datetime
 import requests
 import json
+import random
 
 # --------------------------------------------------------------------------------------------
 # INITIALISATION
@@ -13,13 +14,16 @@ ask = Ask(app, "/alexa")
 ### Global Variables
 quizFile = open("quiz.json","r")
 questions_list = json.load(quizFile)
+query = ""
+correct_answer = ""
+option_str = ""
 
 
 @ask.launch
 def new_ask():
     print("Launch invoked")
-
-    return question("Hi Jatin")
+    welcome = "Welcome to the quiz Show. \n\n Say 'Lets Begin' to start the quiz. and \n\n Goodbye to terminate the quiz."
+    return question(welcome)
 
 
 @ask.intent('personalDetails')
@@ -27,6 +31,33 @@ def personal_details(first_name):
 	print("hello" + first_name + "in personal details section")
 	welcome = render_template('welcome')
 	return question(welcome)
+
+
+@ask.intent("question_intent")
+def display_question():
+	random.shuffle(questions_list)
+	questions_list_item = questions_list[0]
+
+	global query
+	global option_str
+	global answer
+
+	query = questions_list_item["Question"]
+	options = questions_list_item["Options"]
+	option_str = options[0] + "\n\n" + options[1] + "\n\n" +options[2] + "\n\n" +options[3]
+	correct_answer = questions_list_item["Answers"]
+	
+	return question(query + "\n\n" + option_str)
+
+
+@ask.intent("answer_intent")
+def display_answer(user_answer):
+	global correct_answer
+
+	if user_answer == correct_answer:
+		return question("Your answer is right.")
+
+	return question("Correct Answer is Option " + correct_answer)
 
 
 @app.route("/", methods=["GET", "POST"])
